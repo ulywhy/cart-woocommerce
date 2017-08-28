1561386dad54b91a419a5eb96622b1e565b44e58
Marcelo Tomio Hama <hama.marcelo.ti@gmail.com>
2017-08-28 02:25:07 -0300

    rolling back to v2.2.11 to fix css of custom cho v2

--- v2.x/woo-mercado-pago-module/mercadopago/sdk/lib/mercadopago.php
+++ v2.x/woo-mercado-pago-module/mercadopago/sdk/lib/mercadopago.php
@@ -12,7 +12,7 @@ $GLOBALS['LIB_LOCATION'] = dirname( __FILE__ );
 
 class MP {
 
-	private $version = '2.2.12';
+	private $version = '2.2.13';
 	private $client_id;
 	private $client_secret;
 	private $ll_access_token;
