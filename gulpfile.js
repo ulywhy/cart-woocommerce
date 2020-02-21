f0f42d74a460d98cda4e21e784328245f405a325
Fernando Henriques <fernando.henriques@mercadolivre.com>
2020-02-20 23:16:11 -0300

    Minify JSs files with Gulp

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+
+const config = {
+  scripts: [
+    './assets/js/basic_config_mercadopago.js',
+    './assets/js/basic-cho.js',
+    './assets/js/credit-card.js',
+    './assets/js/custom_config_mercadopago.js',
+    './assets/js/ticket_config_mercadopago.js',
+    './assets/js/ticket.js',
+  ],
+};
+
+gulp.task('scripts', function() {
+  return gulp.src(config.scripts)
+    .pipe(uglify())
+    .pipe(rename({ extname: '.min.js' }))
+    .pipe(gulp.dest('./assets/js/'));
+});
+
+gulp.task('default', gulp.series('scripts'));
