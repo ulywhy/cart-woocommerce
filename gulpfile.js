a56db68934a0a9c6c9c9c4db5b10bc3c19bed84b
Fernando Henriques <fernando.henriques@mercadolivre.com>
2020-02-21 08:17:43 -0300

    Create task to run jsHint against all js files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require('gulp');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
+const jshint = require('gulp-jshint');
+const jshintStylish = require('jshint-stylish');
 
 const config = {
   scripts: [
@@ -12,6 +14,13 @@ const config = {
     './assets/js/ticket.js',
   ],
 };
+
+gulp.task('jshint', function() {
+  return gulp.src(config.scripts)
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter(jshintStylish))
+    .pipe(jshint.reporter('fail') );
+});
 
 gulp.task('scripts', function() {
   return gulp.src(config.scripts)
