99b4ca0541f551b9a0cdf13f4b54a1a639e47cac
Giovanni Cavallari <jobacavallari@hotmail.com>
2020-03-23 13:12:13 -0300

    Update plugin version from 4.1.1 to 4.2.0

--- woocommerce-mercadopago.php
+++ woocommerce-mercadopago.php
@@ -4,7 +4,7 @@
  * Plugin Name: Mercado Pago payments for WooCommerce
  * Plugin URI: https://github.com/mercadopago/cart-woocommerce
  * Description: Configure the payment options and accept payments with cards, ticket and money of Mercado Pago account.
- * Version: 4.1.1
+ * Version: 4.2.0
  * Author: Mercado Pago
  * Author URI: https://www.mercadopago.com.br/developers/
  * Text Domain: woocommerce-mercadopago
