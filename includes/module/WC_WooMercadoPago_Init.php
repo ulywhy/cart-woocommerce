9e45a642b9fa1861605c3ecebd10e073e6303936
Fabio Landi <fabio.landi@mercadolivre.com>
2020-08-27 11:28:25 -0300

    it was move funcicton load text domain to class intWC_WooMercadoPago_Init

--- includes/module/WC_WooMercadoPago_Init.php
+++ includes/module/WC_WooMercadoPago_Init.php
@@ -8,6 +8,26 @@ class WC_WooMercadoPago_Init
 {
 
     /**
+     * Load plugin text domain.
+     *
+     * Need to require here before test for PHP version.
+     *
+     * @since 3.0.1
+     */
+    public static function woocommerce_mercadopago_load_plugin_textdomain()
+    {
+        $text_domain = 'woocommerce-mercadopago';
+        $locale = apply_filters('plugin_locale', get_locale(), $text_domain);
+
+        $original_language_file = dirname(__FILE__) . '/i18n/languages/woocommerce-mercadopago-' . $locale . '.mo';
+
+        // Unload the translation for the text domain of the plugin
+        unload_textdomain($text_domain);
+        // Load first the override file
+        load_textdomain($text_domain, $original_language_file);
+    }
+
+    /**
      * Notice about unsupported PHP version.
      *
      * @since 3.0.1
@@ -68,6 +88,7 @@ class WC_WooMercadoPago_Init
     public static function woocommerce_mercadopago_init()
     {
 
+        self::woocommerce_mercadopago_load_plugin_textdomain();
         require_once dirname(__FILE__) . '../../admin/notices/WC_WooMercadoPago_Notices.php';
         WC_WooMercadoPago_Notices::initMercadopagoNnotice();
 
--- woocommerce-mercadopago.php
+++ woocommerce-mercadopago.php
@@ -29,28 +29,6 @@ if (!function_exists('is_plugin_active')) {
     include_once ABSPATH . 'wp-admin/includes/plugin.php';
 }
 
-/**
- * Load plugin text domain.
- *
- * Need to require here before test for PHP version.
- *
- * @since 3.0.1
- */
-function woocommerce_mercadopago_load_plugin_textdomain()
-{
-    $text_domain = 'woocommerce-mercadopago';
-    $locale = apply_filters('plugin_locale', get_locale(), $text_domain);
-
-    $original_language_file = dirname(__FILE__) . '/i18n/languages/woocommerce-mercadopago-' . $locale . '.mo';
-
-    // Unload the translation for the text domain of the plugin
-    unload_textdomain($text_domain);
-    // Load first the override file
-    load_textdomain($text_domain, $original_language_file);
-}
-
-add_action( 'plugins_loaded', 'woocommerce_mercadopago_load_plugin_textdomain' );
-
 if (!class_exists('WC_WooMercadoPago_Init')) {
     include_once dirname(__FILE__) . '/includes/module/WC_WooMercadoPago_Init.php';
     add_action('plugins_loaded', array('WC_WooMercadoPago_Init', 'woocommerce_mercadopago_init'));
